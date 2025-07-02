e974a367ef456faf93c847fc036f3ee9373871b1
ScrewPerfect <130380354+ScrewPerfect@users.noreply.github.com>
2025-07-02 11:20:16 +0800

    Update index.php

--- Admin/index.php
+++ Admin/index.php
@@ -1,10 +1,13 @@
 <?php require_once('header.php'); ?>
 
+
+
 <section class="content-header">
     <h1>Dashboard</h1>
 </section>
 
 <?php
+// Existing queries
 $statement = $pdo->prepare("SELECT * FROM tbl_top_category");
 $statement->execute();
 $total_top_category = $statement->rowCount();
@@ -25,29 +28,52 @@ $statement = $pdo->prepare("SELECT * FROM tbl_customer WHERE cust_status='1'");
 $statement->execute();
 $total_customers = $statement->rowCount();
 
-$statement = $pdo->prepare("SELECT * FROM tbl_shipping_cost");
-$statement->execute();
-$available_shipping = $statement->rowCount();
-
 $statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=?");
 $statement->execute(array('Completed'));
 $total_order_completed = $statement->rowCount();
-
-$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE shipping_status=?");
-$statement->execute(array('Completed'));
-$total_shipping_completed = $statement->rowCount();
 
 $statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=?");
 $statement->execute(array('Pending'));
 $total_order_pending = $statement->rowCount();
 
-$statement = $pdo->prepare("SELECT * FROM tbl_payment WHERE payment_status=? AND shipping_status=?");
-$statement->execute(array('Completed','Pending'));
-$total_order_complete_shipping_pending = $statement->rowCount();
+// New queries for Size and Color
+$statement = $pdo->prepare("SELECT * FROM tbl_size");
+$statement->execute();
+$total_size = $statement->rowCount();
+
+$statement = $pdo->prepare("SELECT * FROM tbl_color");
+$statement->execute();
+$total_color = $statement->rowCount();
+
 ?>
 
 <section class="content">
     <div class="row">
+        <<div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-green">
+                <div class="inner">
+                    <h3><?php echo $total_order_completed; ?></h3>
+                    <p>Completed Orders</p>
+                </div>
+                <div class="icon">
+                    <i class="ionicons ion-android-checkbox-outline"></i>
+                </div>
+               
+            </div>
+        </div>    
+    
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-maroon">
+                <div class="inner">
+                    <h3><?php echo $total_order_pending; ?></h3>
+                    <p>Pending Orders</p>
+                </div>
+                <div class="icon">
+                    <i class="ionicons ion-clipboard"></i>
+                </div>
+                <a href="order.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
         <div class="col-lg-3 col-xs-6">
             <div class="small-box bg-primary">
                 <div class="inner">
@@ -60,59 +86,6 @@ $total_order_complete_shipping_pending = $statement->rowCount();
                 <a href="product.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
-
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-maroon">
-                <div class="inner">
-                    <h3><?php echo $total_order_pending; ?></h3>
-                    <p>Pending Orders</p>
-                </div>
-                <div class="icon">
-                    <i class="ionicons ion-clipboard"></i>
-                </div>
-                <a href="order-pending.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-green">
-                <div class="inner">
-                    <h3><?php echo $total_order_completed; ?></h3>
-                    <p>Completed Orders</p>
-                </div>
-                <div class="icon">
-                    <i class="ionicons ion-android-checkbox-outline"></i>
-                </div>
-                <a href="order-completed.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-aqua">
-                <div class="inner">
-                    <h3><?php echo $total_shipping_completed; ?></h3>
-                    <p>Completed Shipping</p>
-                </div>
-                <div class="icon">
-                    <i class="ionicons ion-checkmark-circled"></i>
-                </div>
-                <a href="order-completed.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-orange">
-                <div class="inner">
-                    <h3><?php echo $total_order_complete_shipping_pending; ?></h3>
-                    <p>Pending Shippings</p>
-                </div>
-                <div class="icon">
-                    <i class="ionicons ion-load-a"></i>
-                </div>
-                <a href="order-pending-shipping.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
         <div class="col-lg-3 col-xs-6">
             <div class="small-box bg-red">
                 <div class="inner">
@@ -125,25 +98,12 @@ $total_order_complete_shipping_pending = $statement->rowCount();
                 <a href="customer.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
-
-        <div class="col-lg-3 col-xs-6">
-            <div class="small-box bg-teal">
-                <div class="inner">
-                    <h3><?php echo $available_shipping; ?></h3>
-                    <p>Available Shippings</p>
-                </div>
-                <div class="icon">
-                    <i class="ionicons ion-location"></i>
-                </div>
-                <a href="shipping-cost.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
-            </div>
-        </div>
-
+        
         <div class="col-lg-3 col-xs-6">
             <div class="small-box bg-olive">
                 <div class="inner">
                     <h3><?php echo $total_top_category; ?></h3>
-                    <p>Top Categories</p>
+                    <p>Main Categories</p>
                 </div>
                 <div class="icon">
                     <i class="ionicons ion-arrow-up-b"></i>
@@ -156,7 +116,7 @@ $total_order_complete_shipping_pending = $statement->rowCount();
             <div class="small-box bg-blue">
                 <div class="inner">
                     <h3><?php echo $total_mid_category; ?></h3>
-                    <p>Mid Categories</p>
+                    <p>Type Categories</p>
                 </div>
                 <div class="icon">
                     <i class="ionicons ion-android-menu"></i>
@@ -169,12 +129,40 @@ $total_order_complete_shipping_pending = $statement->rowCount();
             <div class="small-box bg-maroon">
                 <div class="inner">
                     <h3><?php echo $total_end_category; ?></h3>
-                    <p>End Categories</p>
+                    <p>Sub Categories</p>
                 </div>
                 <div class="icon">
                     <i class="ionicons ion-arrow-down-b"></i>
                 </div>
                 <a href="end-category.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- New Size Category Box -->
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-teal">
+                <div class="inner">
+                    <h3><?php echo $total_size; ?></h3>
+                    <p>Sizes</p>
+                </div>
+                <div class="icon">
+                    <i class="ionicons ion-arrow-resize"></i>
+                </div>
+                <a href="size.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
+            </div>
+        </div>
+
+        <!-- New Color Category Box -->
+        <div class="col-lg-3 col-xs-6">
+            <div class="small-box bg-purple">
+                <div class="inner">
+                    <h3><?php echo $total_color; ?></h3>
+                    <p>Colors</p>
+                </div>
+                <div class="icon">
+                    <i class="ionicons ion-paintbrush"></i>
+                </div>
+                <a href="color.php" class="small-box-footer">More info <i class="fa fa-arrow-circle-right"></i></a>
             </div>
         </div>
     </div>
